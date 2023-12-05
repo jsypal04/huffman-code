f0b5f0b1e14f754c359e046acb86b3920fea3daa
Joseph Sypal <60202320+jsypal04@users.noreply.github.com>
2023-12-04 20:10:33 -0500

    add code to get character frequency

--- Main.java
+++ Main.java
@@ -8,9 +8,9 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static String getCodeTable(Node node, String binary, HashMap<String, String> codeTable) {
+    public static String getCodeTable(Node node, String binary, HashMap<Character, String> codeTable) {
         if (node.getLeftChild() == null && node.getRightChild() == null) {
-            codeTable.put("" + node.getCharacter(), binary);
+            codeTable.put(node.getCharacter(), binary);
             return binary;
         }
         else if (node.getLeftChild() != null && node.getRightChild() == null) {
@@ -46,27 +46,28 @@ public class Main {
         scan.close();
         
         // get character frequencies
-        
+        HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (!frequencies.containsKey(c)) {
+                frequencies.put(c, 1);
+            }
+            else {
+                int frequency = frequencies.get(c);
+                frequency++;
+                frequencies.put(c, frequency);
+            }
+        }
 
         PriorityQueue charQ = new PriorityQueue();
         BinaryTree huffmanTree = new BinaryTree();
-        HashMap<String, String> codeTable = new HashMap<String, String>();
+        HashMap<Character, String> codeTable = new HashMap<Character, String>();
 
-        charQ.enqueue(new Node(2, 'h', null));
-        charQ.enqueue(new Node(2, 'e', null));
-        charQ.enqueue(new Node(3, 'l', null));
-        charQ.enqueue(new Node(5, 'o', null));
-        charQ.enqueue(new Node(2, 'w', null));
-        charQ.enqueue(new Node(2, 'r', null));
-        charQ.enqueue(new Node(2, 'd', null));
-        charQ.enqueue(new Node(5, ' ', null));
-        charQ.enqueue(new Node(1, 'a', null));
-        charQ.enqueue(new Node(1, 'y', null));
-        charQ.enqueue(new Node(1, 'u', null));
-        charQ.enqueue(new Node(1, 'i', null));
-        charQ.enqueue(new Node(1, 'n', null));
-        charQ.enqueue(new Node(1, 'g', null));
-        charQ.enqueue(new Node(1, '?', null));
+        for (Character key : frequencies.keySet()) {
+            charQ.enqueue(new Node(frequencies.get(key), key, null));
+        }
+
+        charQ.print();
 
         while (charQ.getSize() > 1) {
             Node small1 = charQ.dequeue();
@@ -83,27 +84,8 @@ public class Main {
 
         String encodedText = "";
         for (int i = 0; i < text.length(); i++) {
-            encodedText += codeTable.get(text.substring(i, i + 1));
+            encodedText += codeTable.get(text.charAt(i));
         }
         System.out.println(encodedText);
     }
-}
-
-/*hello world how are you doing?
- * 
- *                                          30
- *                                     /           \
- *                                   12               18
- *                                /    \              /       \
- *                             5(o)     7            8             10  
- *                                    /  \         /     \         /  \
- *                                 3(l)  4        4       4        5   5(sp)
- *                                     /   \     / \     / \      /   \
- *                                  2(w)  3(e) 2(d)2(r) 2     2   2(h)   3 
- *                                                     / \    / \       /   \
- *                                                 1(n) 1(i) 1(?) 1(g) 1(a)  2
- *                                                                          / \
- *                                                                       1(u)  1(y)
- * 
- *  
-  */                                                   
+}                                                 
